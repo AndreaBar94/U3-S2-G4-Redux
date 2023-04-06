542d0186cc9373be4fd1bab0b20ea72c478404a4
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-06 22:36:14 +0200

    edit

--- src/components/ButtonToFavourites.jsx
+++ src/components/ButtonToFavourites.jsx
@@ -8,7 +8,7 @@ const ButtonToFavourites = () => {
     <Link to={"/favourites"}>
       <Button variant="success" onClick={() => navigate("/favourites")}>
         Fav
-      </Button>
+      </Button> 
     </Link>
   );
 };
