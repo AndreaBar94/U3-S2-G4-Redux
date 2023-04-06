a696ef25a9ee2027833a15c5f9df1cce7ec1d316
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-06 13:52:22 +0200

    added redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+	favorites: {
+		content: [],
+	},
+};
+
+const mainReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case "ADD_TO_FAVORITES":
+			return {};
+		default:
+			return state;
+	}
+};
+
+export default mainReducer;
