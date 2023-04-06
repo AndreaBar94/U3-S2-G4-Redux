826eef5b678be6fa9c4b18f82a63b5849c24e5e7
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-04-06 17:01:00 +0200

    edit delete

--- src/components/FavouritesPage.jsx
+++ src/components/FavouritesPage.jsx
@@ -1,17 +1,22 @@
-import { ListGroup } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { Button, ListGroup } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const FavouritesPage = () => {
     const favourites = useSelector((state) => state.favourites.content)
+    const dispatch = useDispatch()
+
     return(
         <ListGroup>
             {favourites.length > 0 &&
-            (favourites.map((favourite)=> 
+            (favourites.map((favourite, i)=> 
                 
                     <ListGroup.Item key={favourite._id}>
                             <Link to={`/${favourite.url}`}>{favourite.company_name}</Link>
                             <span className="mx-3">{favourite.title}</span>
+                            <Button className="bg-danger" onClick={(()=>{
+                                dispatch({ type: 'DELETE_FROM_FAVOURITES', payload: i})
+                            })}>Delete</Button>
                     </ListGroup.Item>
                 
             ))
